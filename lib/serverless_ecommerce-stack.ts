2133d22994f1b590952863c92080702095290ccd
Manuel Molina <manuelmolinapena@gmail.com>
2024-09-02 19:18:42 -0600

    [FEAT]: Users infrastructure.

--- lib/serverless_ecommerce-stack.ts
+++ lib/serverless_ecommerce-stack.ts
@@ -4,7 +4,6 @@ import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import * as apiGateway from 'aws-cdk-lib/aws-apigateway'
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ServerlessEcommerceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -15,6 +14,30 @@ export class ServerlessEcommerceStack extends cdk.Stack {
     const productTable = new dynamodb.Table(this, 'ProductsTable', {
       partitionKey: {
         name: 'productId', type: dynamodb.AttributeType.STRING
+      }
+    })
+
+    const userTable = new dynamodb.Table(this, 'UserTable', {
+      partitionKey: {
+        name: 'userId', type: dynamodb.AttributeType.STRING
+      }
+    })
+
+    const createUserHandler = new lambda.Function(this, 'CreateUserHandler', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset('src/lambda/users/create/function.zip'),
+      handler: 'index.handler',
+      environment: {
+        USER_TABLE_NAME: userTable.tableName
+      }
+    })
+
+    const getUsersHandler = new lambda.Function(this, 'GetUsersHandler', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset('src/lambda/users/get/function.zip'),
+      handler: 'index.handler',
+      environment: {
+        USER_TABLE_NAME: userTable.tableName
       }
     })
 
@@ -47,12 +70,31 @@ export class ServerlessEcommerceStack extends cdk.Stack {
       }
     })
 
+    const deleteProductHandler = new lambda.Function(this, 'DeleteProductHandler', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset('src/lambda/products/delete/function.zip'),
+      handler: 'index.handler',
+      environment: {
+        PRODUCT_TABLE_NAME: productTable.tableName,
+        PRODUCT_BUCKET_NAME: productImagesBucket.bucketName
+      }
+    })
+
+    userTable.grantReadWriteData(createUserHandler)
+    userTable.grantReadData(getUsersHandler)
 
     productTable.grantReadWriteData(createProductHandler)
+    productTable.grantReadWriteData(deleteProductHandler)
+    productImagesBucket.grantDelete(deleteProductHandler)
     productImagesBucket.grantReadWrite(createProductHandler)
     productTable.grantReadData(getProductHandler)
     productTable.grantReadWriteData(updateProductHandler)
     productImagesBucket.grantReadWrite(updateProductHandler)
+
+    const userApi = new apiGateway.RestApi(this, 'UserApi')
+    const userResource = userApi.root.addResource('user')
+    userResource.addMethod('POST', new apiGateway.LambdaIntegration(createUserHandler))
+    userResource.addMethod('GET', new apiGateway.LambdaIntegration(getUsersHandler))
 
     const productApi = new apiGateway.RestApi(this, 'ProductApi')
     const productResource = productApi.root.addResource('product')
@@ -60,5 +102,6 @@ export class ServerlessEcommerceStack extends cdk.Stack {
     productResource.addMethod('POST', new apiGateway.LambdaIntegration(createProductHandler))
     productResource.addMethod('GET', new apiGateway.LambdaIntegration(getProductHandler))
     productIdResource.addMethod('PUT', new apiGateway.LambdaIntegration(updateProductHandler))
+    productIdResource.addMethod('DELETE', new apiGateway.LambdaIntegration(deleteProductHandler))
   }
 }
