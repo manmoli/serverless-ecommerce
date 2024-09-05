0b32eaa9837c2fd9101869c0a8dfb6c0c96b66c3
Manuel Molina <manuelmolinapena@gmail.com>
2024-09-04 19:39:30 -0600

    [FEAT]: Authorizer async fix.

--- src/lambda/authorization/auth/index.ts
+++ src/lambda/authorization/auth/index.ts
@@ -1,40 +1,24 @@
-import { AuthResponse, APIGatewayTokenAuthorizerEvent, Context, APIGatewayEventLambdaAuthorizerContext, PolicyDocument, Statement, StatementEffect } from 'aws-lambda'
+import { AuthResponse, APIGatewayTokenAuthorizerEvent, Context, PolicyDocument, Statement, StatementEffect, APIGatewayAuthorizerResultContext } from 'aws-lambda'
 import * as jwt from 'jsonwebtoken'
 
 const JWT_SECRET: string = process.env.JWT_SECRET ?? ''
 
-export const handler = (event: APIGatewayTokenAuthorizerEvent): AuthResponse => {
+export const handler = async (event: APIGatewayTokenAuthorizerEvent) => {
     try {
         const token = event.authorizationToken.split(' ')[1]
         const decoded: jwt.JwtPayload = jwt.verify(token, JWT_SECRET) as jwt.JwtPayload
-        console.log(decoded)
 
-        const policy = generatePolicy(decoded.userId, 'Allow', event.methodArn)
-
-        console.log(policy)
+        const policy = generatePolicy(decoded.userId, 'Allow', event.methodArn, decoded)
 
         return policy
-    } catch (error: any) {
-        console.error('Unauthorized: ', error)
 
-        return {
-            principalId: 'user',
-            policyDocument: {
-                Version: '2012-10-17',
-                Statement: [{
-                    Action: 'execute-api:Invoke',
-                    Effect: 'Deny',
-                    Resource: event.methodArn
-                }]
-            },
-            context: {
-                error: 'Unauthorized'
-            }
-        }
+    } catch (error: unknown) {
+        console.log('Unauthorized: ', error)
+        return generatePolicy('user', 'Deny', event.methodArn)
     }
 }
 
-const generatePolicy = function(principalId: string, effect: StatementEffect, resource: string) {
+const generatePolicy = function (principalId: string, effect: StatementEffect, resource: string, context?: APIGatewayAuthorizerResultContext) {
     const statement: Statement = {
         Action: 'execute-api:Invoke',
         Effect: effect,
@@ -46,15 +30,9 @@ const generatePolicy = function(principalId: string, effect: StatementEffect, re
     };
     const authResponse: AuthResponse = {
         principalId: principalId,
-        policyDocument: policyDocument
-        
+        policyDocument: policyDocument,
+        context: context
     }
-    
-    authResponse.context = {
-        "stringKey": "stringval",
-        "numberKey": 123,
-        "booleanKey": true
-    };
 
     return authResponse;
 }
